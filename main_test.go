8e1bab67a278a93ca56aad2af12875187dd5ec05
Vaughn Iverson <vsivsi@yahoo.com>
2019-04-10 19:04:55 -0700

    Streamlined tests with helpers

--- main_test.go
+++ main_test.go
@@ -18,244 +18,165 @@ func TestPow(t *testing.T) {
 	Inf := big.NewFloat(0.0).SetInf(false)
 	NegInf := big.NewFloat(0.0).SetInf(true)
 
-	t.Run("X=-Inf,n=0", func(t *testing.T) {
-		if Pow(Inf, 0).Cmp(One) != 0 {
-			t.FailNow()
+	bigAssert := func(got, want *big.Float, t *testing.T) {
+		t.Helper()
+		if got.Cmp(want) != 0 {
+			t.Fatal("Wanted:", want, "Got:", got)
 		}
+	}
+
+	bigAssertTol := func(got *big.Float, want, tol float64, t *testing.T) {
+		t.Helper()
+		gotFloat, _ := got.Float64()
+		if math.Abs(gotFloat-want)/(gotFloat+want) > tol {
+			t.Fatal("Wanted:", want, "Got:", gotFloat, "Tolerance:", tol)
+		}
+	}
+
+	t.Run("Pow(Inf, 0)", func(t *testing.T) {
+		bigAssert(Pow(Inf, 0), One, t)
 	})
-	t.Run("X=+Inf,n=1", func(t *testing.T) {
-		if Pow(Inf, 1).Cmp(Inf) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Inf, 1)", func(t *testing.T) {
+		bigAssert(Pow(Inf, 1), Inf, t)
 	})
-	t.Run("X=+Inf,n=2", func(t *testing.T) {
-		if Pow(Inf, 2).Cmp(Inf) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Inf, 2)", func(t *testing.T) {
+		bigAssert(Pow(Inf, 2), Inf, t)
 	})
-	t.Run("X=+Inf,n=-1", func(t *testing.T) {
-		if Pow(Inf, -1).Cmp(Zero) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Inf, -1)", func(t *testing.T) {
+		bigAssert(Pow(Inf, -1), Zero, t)
 	})
-	t.Run("X=+Inf,n=-2", func(t *testing.T) {
-		if Pow(Inf, -2).Cmp(Zero) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Inf, -2)", func(t *testing.T) {
+		bigAssert(Pow(Inf, -2), Zero, t)
 	})
 
-	t.Run("X=-Inf,n=0", func(t *testing.T) {
-		if Pow(NegInf, 0).Cmp(Pow(NegZero, 0)) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegInf, 0)", func(t *testing.T) {
+		bigAssert(Pow(NegInf, 0), Pow(NegZero, 0), t)
 	})
-	t.Run("X=-Inf,n=1", func(t *testing.T) {
-		if Pow(NegInf, 1).Cmp(Pow(NegZero, -1)) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegInf, 1)", func(t *testing.T) {
+		bigAssert(Pow(NegInf, 1), Pow(NegZero, -1), t)
 	})
-	t.Run("X=-Inf,n=2", func(t *testing.T) {
-		if Pow(NegInf, 2).Cmp(Pow(NegZero, -2)) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegInf, 2)", func(t *testing.T) {
+		bigAssert(Pow(NegInf, 2), Pow(NegZero, -2), t)
 	})
-	t.Run("X=-Inf,n=-1", func(t *testing.T) {
-		if Pow(NegInf, -1).Cmp(Pow(NegZero, 1)) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegInf, -1)", func(t *testing.T) {
+		bigAssert(Pow(NegInf, -1), Pow(NegZero, 1), t)
 	})
-	t.Run("X=-Inf,n=-2", func(t *testing.T) {
-		if Pow(NegInf, -2).Cmp(Pow(NegZero, 2)) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegInf, -2)", func(t *testing.T) {
+		bigAssert(Pow(NegInf, -2), Pow(NegZero, 2), t)
 	})
 
-	t.Run("X=0.0,n=0", func(t *testing.T) {
-		if Pow(Zero, 0).Cmp(One) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Zero, 0)", func(t *testing.T) {
+		bigAssert(Pow(Zero, 0), One, t)
 	})
-	t.Run("X=0.0,n=1", func(t *testing.T) {
-		if Pow(Zero, 1).Cmp(Zero) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Zero, 1)", func(t *testing.T) {
+		bigAssert(Pow(Zero, 1), Zero, t)
 	})
-	t.Run("X=0.0,n=2", func(t *testing.T) {
-		if Pow(Zero, 2).Cmp(Zero) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Zero, 2)", func(t *testing.T) {
+		bigAssert(Pow(Zero, 2), Zero, t)
 	})
-	t.Run("X=0.0,n=-1", func(t *testing.T) {
-		if Pow(Zero, -1).Cmp(Inf) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Zero, -1)", func(t *testing.T) {
+		bigAssert(Pow(Zero, -1), Inf, t)
 	})
-	t.Run("X=0.0,n=-2", func(t *testing.T) {
-		if Pow(Zero, -2).Cmp(Inf) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Zero, -2)", func(t *testing.T) {
+		bigAssert(Pow(Zero, -2), Inf, t)
 	})
 
-	t.Run("X=-0.0,n=0", func(t *testing.T) {
-		if Pow(NegZero, 0).Cmp(One) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegZero, 0)", func(t *testing.T) {
+		bigAssert(Pow(NegZero, 0), One, t)
 	})
-	t.Run("X=-0.0,n=1", func(t *testing.T) {
-		if Pow(NegZero, 1).Cmp(NegZero) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegZero, 1)", func(t *testing.T) {
+		bigAssert(Pow(NegZero, 1), NegZero, t)
 	})
-	t.Run("X=-0.0,n=2", func(t *testing.T) {
-		if Pow(NegZero, 2).Cmp(Zero) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegZero, 2)", func(t *testing.T) {
+		bigAssert(Pow(NegZero, 2), Zero, t)
 	})
-	t.Run("X=-0.0,n=-1", func(t *testing.T) {
-		if Pow(NegZero, -1).Cmp(NegInf) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegZero, -1)", func(t *testing.T) {
+		bigAssert(Pow(NegZero, -1), NegInf, t)
 	})
-	t.Run("X=-0.0,n=-2", func(t *testing.T) {
-		if Pow(NegZero, -2).Cmp(Inf) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegZero, -2)", func(t *testing.T) {
+		bigAssert(Pow(NegZero, -2), Inf, t)
 	})
 
-	t.Run("X=1.0,n=0", func(t *testing.T) {
-		if Pow(One, 0).Cmp(One) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(One, 0)", func(t *testing.T) {
+		bigAssert(Pow(One, 0), One, t)
 	})
-	t.Run("X=1.0,n=1", func(t *testing.T) {
-		if Pow(One, 1).Cmp(One) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(One, 1)", func(t *testing.T) {
+		bigAssert(Pow(One, 1), One, t)
 	})
-	t.Run("X=1.0,n=2", func(t *testing.T) {
-		if Pow(One, 2).Cmp(One) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(One, 2)", func(t *testing.T) {
+		bigAssert(Pow(One, 2), One, t)
 	})
-	t.Run("X=1.0,n=-1", func(t *testing.T) {
-		if Pow(One, -1).Cmp(One) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(One, -1)", func(t *testing.T) {
+		bigAssert(Pow(One, -1), One, t)
 	})
-	t.Run("X=1.0,n=-2", func(t *testing.T) {
-		if Pow(One, -2).Cmp(One) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(One, -2)", func(t *testing.T) {
+		bigAssert(Pow(One, -2), One, t)
 	})
 
-	t.Run("X=-1.0,n=0", func(t *testing.T) {
-		if Pow(NegOne, 0).Cmp(One) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegOne, 0)", func(t *testing.T) {
+		bigAssert(Pow(NegOne, 0), One, t)
 	})
-	t.Run("X=-1.0,n=1", func(t *testing.T) {
-		if Pow(NegOne, 1).Cmp(NegOne) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegOne, 1)", func(t *testing.T) {
+		bigAssert(Pow(NegOne, 1), NegOne, t)
 	})
-	t.Run("X=-1.0,n=2", func(t *testing.T) {
-		if Pow(NegOne, 2).Cmp(One) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegOne, 2)", func(t *testing.T) {
+		bigAssert(Pow(NegOne, 2), One, t)
 	})
-	t.Run("X=-1.0,n=-1", func(t *testing.T) {
-		if Pow(NegOne, 1).Cmp(NegOne) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegOne, 1)", func(t *testing.T) {
+		bigAssert(Pow(NegOne, 1), NegOne, t)
 	})
-	t.Run("X=-1.0,n=-2", func(t *testing.T) {
-		if Pow(NegOne, 2).Cmp(One) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(NegOne, 2)", func(t *testing.T) {
+		bigAssert(Pow(NegOne, 2), One, t)
 	})
 
-	t.Run("X=2.0,n=0", func(t *testing.T) {
-		if Pow(Two, 0).Cmp(One) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Two, 0)", func(t *testing.T) {
+		bigAssert(Pow(Two, 0), One, t)
 	})
-	t.Run("X=2.0,n=1", func(t *testing.T) {
-		if Pow(Two, 1).Cmp(Two) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Two, 1)", func(t *testing.T) {
+		bigAssert(Pow(Two, 1), Two, t)
 	})
-	t.Run("X=2.0,n=2", func(t *testing.T) {
-		if Pow(Two, 2).Cmp(big.NewFloat(0).Mul(Two, Two)) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Two, 2)", func(t *testing.T) {
+		bigAssert(Pow(Two, 2), big.NewFloat(0).Mul(Two, Two), t)
 	})
-	t.Run("X=2.0,n=-1", func(t *testing.T) {
-		if Pow(Two, -1).Cmp(big.NewFloat(0).Quo(One, Two)) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Two, -1)", func(t *testing.T) {
+		bigAssert(Pow(Two, -1), big.NewFloat(0).Quo(One, Two), t)
 	})
-	t.Run("X=2.0,n=-2", func(t *testing.T) {
-		if Pow(Two, -2).Cmp(big.NewFloat(0).Quo(One, big.NewFloat(0).Mul(Two, Two))) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Two, -2)", func(t *testing.T) {
+		bigAssert(Pow(Two, -2), big.NewFloat(0).Quo(One, big.NewFloat(0).Mul(Two, Two)), t)
 	})
 
-	t.Run("X=10.0,n=2", func(t *testing.T) {
-		if Pow(Ten, 2).Cmp(big.NewFloat(math.Pow(10.0, 2))) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Ten, 2)", func(t *testing.T) {
+		bigAssert(Pow(Ten, 2), big.NewFloat(math.Pow(10.0, 2)), t)
 	})
-	t.Run("X=10.0,n=5", func(t *testing.T) {
-		if Pow(Ten, 5).Cmp(big.NewFloat(math.Pow(10.0, 5))) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Ten, 5)", func(t *testing.T) {
+		bigAssert(Pow(Ten, 5), big.NewFloat(math.Pow(10.0, 5)), t)
 	})
-	t.Run("X=10.0,n=25", func(t *testing.T) {
-		if Pow(Ten, 25).Cmp(big.NewFloat(math.Pow(10.0, 25))) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Ten, 25)", func(t *testing.T) {
+		bigAssert(Pow(Ten, 25), big.NewFloat(math.Pow(10.0, 25)), t)
 	})
-	t.Run("X=10.0,n=250", func(t *testing.T) {
-		if Pow(Ten, 250).Cmp(big.NewFloat(math.Pow(10.0, 250))) != 0 {
-			t.FailNow()
-		}
+	t.Run("Pow(Ten, 250)", func(t *testing.T) {
+		bigAssert(Pow(Ten, 250), big.NewFloat(math.Pow(10.0, 250)), t)
 	})
 
 	// These don't work as equalities because the two libraries calculate negative exponents differently
 
-	t.Run("X=10.0,n=-2", func(t *testing.T) {
-		a, _ := Pow(Ten, -2).Float64()
-		b := math.Pow(10.0, -2)
-		if math.Abs(a-b)/(a+b) > 1.0e-13 {
-			t.FailNow()
-		}
+	t.Run("Pow(Ten, -2)", func(t *testing.T) {
+		bigAssertTol(Pow(Ten, -2), math.Pow(10.0, -2), 1.0e-13, t)
 	})
-	t.Run("X=10.0,n=-5", func(t *testing.T) {
-		a, _ := Pow(Ten, -5).Float64()
-		b := math.Pow(10.0, -5)
-		if math.Abs(a-b)/(a+b) > 1.0e-13 {
-			t.FailNow()
-		}
+	t.Run("Pow(Ten, -5)", func(t *testing.T) {
+		bigAssertTol(Pow(Ten, -5), math.Pow(10.0, -5), 1.0e-13, t)
 	})
-	t.Run("X=10.0,n=-25", func(t *testing.T) {
-		a, _ := Pow(Ten, -25).Float64()
-		b := math.Pow(10.0, -25)
-		if math.Abs(a-b)/(a+b) > 1.0e-13 {
-			t.FailNow()
-		}
+	t.Run("Pow(Ten, -25)", func(t *testing.T) {
+		bigAssertTol(Pow(Ten, -25), math.Pow(10.0, -25), 1.0e-13, t)
 	})
-	t.Run("X=10.0,n=-250", func(t *testing.T) {
-		a, _ := Pow(Ten, -250).Float64()
-		b := math.Pow(10.0, -250)
-		if math.Abs(a-b)/(a+b) > 1.0e-13 {
-			t.FailNow()
-		}
+	t.Run("Pow(Ten, -250)", func(t *testing.T) {
+		bigAssertTol(Pow(Ten, -250), math.Pow(10.0, -250), 1.0e-13, t)
 	})
 }
 
 func TestPMF(t *testing.T) {
 
-	BinomialPMF := func(ρ float64, n int64) func(k int64) float64 {
+	binomialPMF := func(ρ float64, n int64) func(k int64) float64 {
 		return func(k int64) float64 {
 			p := math.Pow(ρ, float64(k)) * math.Pow(1-ρ, float64(n-k))
 			p *= math.Gamma(float64(n+1)) / (math.Gamma(float64(k+1)) * math.Gamma(float64(n-k+1)))
@@ -263,83 +184,56 @@ func TestPMF(t *testing.T) {
 		}
 	}
 
-	BinError := func(p float64, n int64) float64 {
-		FloatPMF := BinomialPMF(p, n)
-		BigPMF := PMF(p, n)
+	assertPMFTol := func(p float64, n int64, tol float64, t *testing.T) {
+		t.Helper()
+		floatPMF := binomialPMF(p, n)
+		bigPMF := PMF(p, n)
 		err := 0.0
 		for x := int64(0); x <= n; x++ {
-			err += math.Abs(BigPMF(x) - FloatPMF(x))
+			err += math.Abs(bigPMF(x) - floatPMF(x))
 		}
-		return err
+		if err > tol {
+			t.Fatal("Error:", err, "Tolerance:", tol)
+		}
 	}
 
-	// t.Run("p=0.15,n=10", func(t *testing.T) {
-	//    if BinError(0.15, 10) > 1.0e-14 {
-	//       t.FailNow()
-	//    }
-	// })
-
-	t.Run("p=0.5,n=2", func(t *testing.T) {
-		if BinError(0.5, 2) > 1.0e-14 {
-			t.FailNow()
-		}
+	t.Run("PMF(0.5, 2)", func(t *testing.T) {
+		assertPMFTol(0.5, 2, 1.0e-14, t)
 	})
-	t.Run("p=0.5,n=20", func(t *testing.T) {
-		if BinError(0.5, 20) > 1.0e-14 {
-			t.FailNow()
-		}
+	t.Run("PMF(0.5, 20)", func(t *testing.T) {
+		assertPMFTol(0.5, 20, 1.0e-14, t)
 	})
-	t.Run("p=0.5,n=200", func(t *testing.T) {
-		if BinError(0.5, 200) > 1.0e-14 {
-			t.FailNow()
-		}
+	t.Run("PMF(0.5, 200)", func(t *testing.T) {
+		assertPMFTol(0.5, 200, 1.0e-14, t)
 	})
 
-	t.Run("p=0.15,n=2", func(t *testing.T) {
-		if BinError(0.15, 2) > 1.0e-14 {
-			t.FailNow()
-		}
+	t.Run("PMF(0.15, 2)", func(t *testing.T) {
+		assertPMFTol(0.15, 2, 1.0e-14, t)
 	})
-	t.Run("p=0.15,n=20", func(t *testing.T) {
-		if BinError(0.15, 20) > 1.0e-14 {
-			t.FailNow()
-		}
+	t.Run("PMF(0.15, 20)", func(t *testing.T) {
+		assertPMFTol(0.15, 20, 1.0e-14, t)
 	})
-	t.Run("p=0.15,n=200", func(t *testing.T) {
-		if BinError(0.15, 200) > 1.0e-14 {
-			t.FailNow()
-		}
+	t.Run("PMF(0.15, 200)", func(t *testing.T) {
+		assertPMFTol(0.15, 200, 1.0e-14, t)
 	})
 
-	t.Run("p=0.05,n=2", func(t *testing.T) {
-		if BinError(0.5, 2) > 1.0e-14 {
-			t.FailNow()
-		}
+	t.Run("PMF(0.5, 2)", func(t *testing.T) {
+		assertPMFTol(0.5, 2, 1.0e-14, t)
 	})
-	t.Run("p=0.05,n=20", func(t *testing.T) {
-		if BinError(0.5, 20) > 1.0e-14 {
-			t.FailNow()
-		}
+	t.Run("PMF(0.5, 20)", func(t *testing.T) {
+		assertPMFTol(0.5, 20, 1.0e-14, t)
 	})
-	t.Run("p=0.05,n=200", func(t *testing.T) {
-		if BinError(0.5, 200) > 1.0e-14 {
-			t.FailNow()
-		}
+	t.Run("PMF(0.5, 200)", func(t *testing.T) {
+		assertPMFTol(0.5, 200, 1.0e-14, t)
 	})
 
-	t.Run("p=0.0005,n=2", func(t *testing.T) {
-		if BinError(0.5, 2) > 1.0e-14 {
-			t.FailNow()
-		}
+	t.Run("PMF(0.5, 2)", func(t *testing.T) {
+		assertPMFTol(0.5, 2, 1.0e-14, t)
 	})
-	t.Run("p=0.0005,n=20", func(t *testing.T) {
-		if BinError(0.5, 20) > 1.0e-14 {
-			t.FailNow()
-		}
+	t.Run("PMF(0.5, 20)", func(t *testing.T) {
+		assertPMFTol(0.5, 20, 1.0e-14, t)
 	})
-	t.Run("p=0.005,n=200", func(t *testing.T) {
-		if BinError(0.5, 200) > 1.0e-14 {
-			t.FailNow()
-		}
+	t.Run("PMF(0.5, 200)", func(t *testing.T) {
+		assertPMFTol(0.5, 200, 1.0e-14, t)
 	})
 }
