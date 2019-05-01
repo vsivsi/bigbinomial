c1570a4fb93385d8672fd1abcf5bff6c17e5eaee
Vaughn Iverson <vsivsi@yahoo.com>
2019-05-01 13:43:20 -0700

    Fix intro documentation

--- doc.go
+++ doc.go
@@ -2,10 +2,6 @@
 Package bigbinomial implements binomial distribution PMF and CDF functions with math/big support
 
 This package was created to make it possible to calculate binomial distributions for larger
-numbers of trials than are possible using IEEE floating point math.
-
-However, as shown in the example below, this approach does not work for distributions with large
-numbers of trials because the IEEE floating point can't represent large enough
-values to complete the necessary calculations.
+numbers of trials than are possible using IEEE floating point math, as shown in the example below.
 */
 package bigbinomial
