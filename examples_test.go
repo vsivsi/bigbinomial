1193263be549068473f1ff4ac76c63a955cb5b9c
Vaughn Iverson <vsivsi@yahoo.com>
2019-05-01 12:02:43 -0700

    Improve example code outputs

--- examples_test.go
+++ examples_test.go
@@ -20,34 +20,34 @@ func Example() {
 
 	pmf := binomialPMF(0.5, 100) // Return a function to calculate PMF(0.5, 100, k)
 	prob := pmf(50)              // prob = 0.07958923738717867, about 8%
-	fmt.Println(prob)
+	fmt.Println("PMF(0.5, 100, 50) =", prob)
 
 	// But what if we want the answer for 500 heads out of 1000 flips?
 	// The float64 implementation breaks for large values of n
 
 	pmf = binomialPMF(0.5, 1000)
 	prob = pmf(500) // prob == NaN  (Failure!)
-	fmt.Println(prob)
+	fmt.Println("PMF(0.5, 1000, 500) =", prob)
 
 	// Output:
-	// 0.07958923738717867
-	// NaN
+	// PMF(0.5, 100, 50) = 0.07958923738717867
+	// PMF(0.5, 1000, 500) = NaN
 }
 
 func ExamplePMF() {
 	pmf, _ := bigbinomial.PMF(0.5, 1000)
 	prob := pmf(500)
-	fmt.Println(prob)
+	fmt.Println("PMF(0.5, 1000, 500) =", prob)
 
 	// Output:
-	// 0.0252250181783608
+	// PMF(0.5, 1000, 500) = 0.0252250181783608
 }
 
 func ExampleCDF() {
 	cdf, _ := bigbinomial.CDF(0.5, 1000)
 	prob := cdf(500)
-	fmt.Println(prob)
+	fmt.Println("CDF(0.5, 1000, 500) =", prob)
 
 	// Output:
-	// 0.5126125090891803
+	// CDF(0.5, 1000, 500) = 0.5126125090891803
 }
