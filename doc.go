cab82b2889284b2eed1de1b8da15554715efe149
Vaughn Iverson <vsivsi@yahoo.com>
2019-05-01 13:48:44 -0700

    Revert package description

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-Bigbinomial implements binomial distribution PMF and CDF functions with math/big support
+Package bigbinomial implements binomial distribution PMF and CDF functions with math/big support
 
 This package was created to make it possible to calculate binomial distributions for larger
 numbers of trials than are possible using IEEE floating point math, as shown in the example below.
